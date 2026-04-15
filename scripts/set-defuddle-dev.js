0dd53265f2edc5e842692db203eedf1ed10fcfa8
Sancerro <karaerbaris@gmail.com>
2026-04-15 21:53:06 +0300

    chore: absorb defuddle into packages/defuddle as subtree

--- scripts/set-defuddle-dev.js
+++ scripts/set-defuddle-dev.js
@@ -6,7 +6,7 @@ const packageJsonPath = path.join(__dirname, '..', 'package.json');
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 
 // Update the defuddle dependency to use local version
-packageJson.dependencies.defuddle = 'file:../defuddle';
+packageJson.dependencies.defuddle = 'file:packages/defuddle';
 
 // Write the updated package.json
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, '\t') + '\n');
